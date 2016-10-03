0035b5a891c002a423ee99b429f98400d954e71d
JustTheOne <394185631@qq.com>
2016-10-03 11:50:25 +0800

    zky

--- app/src/main/java/com/mall/MainActivity.java
+++ app/src/main/java/com/mall/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //测试团队合作
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/mall/zky/JustOneTest.java
@@ -0,0 +1,8 @@
+package com.mall.zky;
+
+/**
+ * Created by Administrator on 2016/10/3.
+ */
+
+public class JustOneTest {
+}
