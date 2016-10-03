923c2dca228d0a674ec3607180da4a5d51d7779d
hasee <42980151@qq.com>
2016-10-03 15:24:55 +0800

    10:3   15 24    qi

--- app/src/main/java/com/mall/zky/GuidActivity.java
+++ app/src/main/java/com/mall/zky/GuidActivity.java
@@ -2,6 +2,7 @@ package com.mall.zky;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -25,11 +26,24 @@ public class GuidActivity extends AppCompatActivity implements ViewPager.OnPageC
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_guid);
         initData();
         initPoints();
         initView();
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     private void initData() {
@@ -123,6 +137,7 @@ public class GuidActivity extends AppCompatActivity implements ViewPager.OnPageC
             for (ImageView iv : mvs) {
                 iv.setVisibility(View.GONE);
             }
+
             SharedPreferences sp = getSharedPreferences("args", MODE_PRIVATE);
             SharedPreferences.Editor editor = sp.edit();
             editor.putBoolean("isGuid", true).commit();
--- app/src/main/java/com/mall/zky/SplashActivity.java
+++ app/src/main/java/com/mall/zky/SplashActivity.java
@@ -2,8 +2,13 @@ package com.mall.zky;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 
 import com.mall.MainActivity;
@@ -14,9 +19,23 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_splash);
         init();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     Intent intent = null;
