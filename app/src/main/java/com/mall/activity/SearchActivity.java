b992fac6c960356849e58cd77c725ff3a8fc0288
Lin <1244118180@qq.com>
2016-10-05 16:58:51 +0800

    lb

--- app/src/main/java/com/mall/activity/SearchActivity.java
+++ app/src/main/java/com/mall/activity/SearchActivity.java
@@ -1,6 +1,8 @@
 package com.mall.activity;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -10,51 +12,70 @@ import android.widget.GridLayout;
 import android.widget.GridView;
 
 import com.mall.R;
+import com.mall.fragment_mall.VPAdapter;
+import com.mall.fragment_search.BrandFragment;
+import com.mall.fragment_search.ClassifyFragment;
+import com.mall.fragment_search.EffectFragment;
 
 import java.util.ArrayList;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
 
     private SearchView searchView;
-    private GridView grid;
+    ArrayList<String> ts = new ArrayList<>();
+    ArrayList<Fragment> vs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        initDatas();
         initView();
+    }
+
+    private void initDatas() {
+        ts.add("分类");
+        ts.add("品牌");
+        ts.add("功效");
+        vs.add(new ClassifyFragment());
+        vs.add(new BrandFragment());
+        vs.add(new EffectFragment());
     }
 
     private void initView() {
         searchView = (SearchView) findViewById(R.id.search_view);
         searchView.setQueryHint("搜索商品，品牌");
         TabLayout tab = (TabLayout) findViewById(R.id.search_table);
-        tab.addTab(tab.newTab().setText("分类"));
-        tab.addTab(tab.newTab().setText("品牌"));
-        tab.addTab(tab.newTab().setText("功效"));
+        tab.setOnTabSelectedListener(this);
+        for (int i = 0; i < ts.size(); i++) {
+            tab.addTab(tab.newTab().setText(ts.get(i)));
+        }
 
-        grid = (GridView) findViewById(R.id.search_grid);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
-        ArrayList<String> datas = new ArrayList<>();
-        datas.add("洁面");
-        datas.add("化妆水");
-        datas.add("乳液/面霜");
-        datas.add("防晒");
-        datas.add("眼妆");
-        datas.add("面膜");
-        datas.add("面部精华");
-        datas.add("T区护理");
-        datas.add("润唇膏");
-        datas.add("男士护理");
-        datas.add("护肤套装");
-        datas.add("其他护肤品");
 
-        adapter.addAll(datas);
-        grid.setAdapter(adapter);
+        ViewPager vp = (ViewPager) findViewById(R.id.search_vp);
+        vp.setAdapter(new VPAdapter(getSupportFragmentManager(),ts,vs));
+
+        tab.setupWithViewPager(vp);
+
 
     }
 
     public void search(View view) {
         searchView.setQuery("",false);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        int position = tab.getPosition();
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 }
